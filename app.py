88f8a70c6c752d20add86cbebbd8ee0591ba1772
ifrah c <saalimifrah9916@gmail.com>
2025-05-19 19:33:55 +0530

    Fix increased password and email field sizes

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ migrate=Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     notes = db.relationship('Note', backref='owner', lazy=True)
 
 # Note Model
